8868148768b78055b46adca3585e0f0a19d66959
iaasif <imtiazasif.cse@gmail.com>
2024-06-25 16:54:37 +0600

    tan stack query is added in main jsx

--- client/src/hooks/useAxiosSecure.jsx
+++ client/src/hooks/useAxiosSecure.jsx
@@ -1,14 +1,33 @@
-// import React from 'react';
-import { axios } from "axios";
+import axios from "axios";
 import { createContext } from "react";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router-dom";
 
 const axiosSecure = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
-
 const useAxiosSecure = () => {
+  const { logOut } = useAuth();
+  const navigate = useNavigate;
+
+  //interceptor
+  axiosSecure.interceptors.response.use(
+    (res) => {
+      console.log("i am form interceptor --->> ",res);
+      return res;
+    },
+    async (error) => {
+      console.log("error form interceptors", error.response);
+      if (error.response.status === 401 || error.response.status === 403) {
+        await logOut();
+        navigate("/login");
+      }
+      return Promise.reject(error);
+    }
+  );
+  // axios.interceptors.request;
   return axiosSecure;
 };
 
